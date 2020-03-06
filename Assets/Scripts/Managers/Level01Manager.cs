26d0301d37c328e288cc0d959f06305ca0a1a749
Joao Vitor Prates <jv-prates@hotmail.com>
2020-03-06 10:16:55 -0300

    aula incompleta

---
+++ Assets/Scripts/Managers/Level01Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level01Manager : MonoBehaviour
+{
+    public Transform[] badSpawn;
+    public GameObject[] badPrefabs;
+    public int badWaves = 3;
+    public float waitTimeFirstWave = 3f;
+    public float waitTimeBetweenWaves = 10f;
+
+    void Start()
+    {
+        
+    }
+
+IEnumerator SpawnBad()
+{
+    yield return new WaitForSeconds(waitTimeFirstWave);
+    for (int i=0; i)
+}
+
+
+}
