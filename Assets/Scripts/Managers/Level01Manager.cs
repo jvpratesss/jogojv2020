e4f0fcd4d74d9c0645de3e242bfc5df272270226
Joao Vitor Prates <jv-prates@hotmail.com>
2020-03-10 11:46:29 -0300

    Creep spawn

--- Assets/Scripts/Managers/Level01Manager.cs
+++ Assets/Scripts/Managers/Level01Manager.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class Level01Manager : MonoBehaviour
 {
     public Transform[] badSpawn;
-    public GameObject[] badPrefabs;
+    public GameObject badPrefabs;
     public int badWaves = 3;
-    public float waitTimeFirstWave = 3f;
-    public float waitTimeBetweenWaves = 10f;
+    public float waitTimeFirstWave = 2f;
+    public float waitTimeBetweenWaves = 4f;
 
     void Start()
     {
-        
+        StartCoroutine(SpawnBad());
     }
 
 IEnumerator SpawnBad()
 {
     yield return new WaitForSeconds(waitTimeFirstWave);
-    for (int i=0; i)
+    for (int i=0; i < badWaves; i++)
+    {
+        for (int j = 0; j < badSpawn.Length; j++)
+        {
+            Instantiate(badPrefabs, badSpawn[j].position, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(waitTimeBetweenWaves);
+    }
 }
 
 
